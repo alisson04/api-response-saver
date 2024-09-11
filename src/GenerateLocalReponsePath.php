a6f17822f79a3bee4793a2725ae1fc7736ca90be
Alisson Fernandes <alissonvinicios04@gmail.com>
2024-09-10 22:17:13 -0300

    [feat] added class GenerateLocalResponsePath and some tests

---
+++ src/GenerateLocalReponsePath.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Alisson04\ApiResponseSaver;
+
+class GenerateLocalReponsePath
+{
+    private array $mapUris;
+    private array $mapParams;
+    private array $mapUriNecessaryParams;
+
+    public function __construct($mapUris, $mapParams, $mapUriNecessaryParams) {
+        $this->mapUris = $mapUris;
+        $this->mapParams = $mapParams;
+        $this->mapUriNecessaryParams = $mapUriNecessaryParams;
+    }
+
+    public function run(string $uri, array $formParams = []): string
+    {
+        $this->validateUri($uri, $formParams);
+        $this->validateParams($uri, $formParams);
+
+        $filePath = $this->mapUris[$uri];
+
+        foreach ($formParams as $key => $value) {
+            $filePath .= $this->mapParams[$key] . "/{$value}/";
+        }
+
+        return $filePath;
+    }
+
+    private function validateUri(string $uri): void
+    {
+        $invalidUri = ! isset($this->mapUris[$uri]);
+
+        if ($invalidUri) {
+            throw new \Exception("Uri not found: {$uri}");
+        }
+    }
+
+    private function validateParams(string $uri, array $formParams): void
+    {
+        $formParamsKeys = array_keys($formParams);
+
+        $invalidParams = array_diff($formParamsKeys, $this->mapParams);
+        if (! empty($invalidParams)) {
+            throw new \Exception("Invalid params found: " . implode(', ', $invalidParams));
+        }
+
+        $uriNecessaryParams = $this->mapUriNecessaryParams[$uri];
+
+        $invalidParamsForUri = array_diff($formParamsKeys, $uriNecessaryParams);
+        if (! empty($invalidParamsForUri)) {
+            throw new \Exception("Invalid params for uri({$uri}) was found: " . implode(', ', $invalidParamsForUri));
+        }
+
+        $missingRequiredParams = array_diff($uriNecessaryParams, $formParamsKeys);
+        if (! empty($missingRequiredParams)) {
+            throw new \Exception("Missing params for uri({$uri}): " . implode(', ', $missingRequiredParams));
+        }
+    }
+}
---
+++ tests/Feature/GenerateLocalResponsePathTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use Alisson04\ApiResponseSaver\GenerateLocalReponsePath;
+
+beforeEach(function () {
+    $mapUris = [
+        'ConsultarTabelaDeReferencia' => 'references',
+        'ConsultarMarcas' => 'brands',
+        'ConsultarModelos' => 'models',
+        'ConsultarAnoModelo' => 'year-model',
+        'ConsultarValorComTodosParametros' => 'prices',
+    ];
+
+    $mapParams = [
+        'codigoTabelaReferencia' => 'reference',
+        'codigoTipoVeiculo' => 'vehicle-type',
+    ];
+
+    $mapUriNecessaryParams = [
+        'ConsultarTabelaDeReferencia' => [],
+        'ConsultarMarcas' => ['codigoTabelaReferencia', 'codigoTipoVeiculo'],
+    ];
+
+    $this->service = new GenerateLocalReponsePath($mapUris, $mapParams, $mapUriNecessaryParams);
+});
+
+it('should generate path to references', function () {
+    $path = $this->service->run('ConsultarTabelaDeReferencia', []);
+    expect($path)->toEqual('references');
+});
+
+it('throws exception by wrong uri', function () {
+    $this->service->run('ConsultarTabelaDeReferencia1', []);
+})->throws('Uri not found: ConsultarTabelaDeReferencia1');
+
+it('throws exception by wrong params', function () {
+    $this->service->run('ConsultarTabelaDeReferencia', ['wrongParam' => 'wrongValue', 'reference' => 1]);
+})->throws('Invalid params found: wrongParam');
+
+it('throws exception by wrong params for uri', function () {
+    $this->service->run('ConsultarTabelaDeReferencia', ['reference' => 1]);
+})->throws('Invalid params for uri(ConsultarTabelaDeReferencia) was found: reference');
+
+it('throws exception by missing params for uri', function () {
+    $path = $this->service->run('ConsultarMarcas', []);
+    expect($path)->toEqual('references');
+})->throws('Missing params for uri(ConsultarMarcas): codigoTabelaReferencia, codigoTipoVeiculo');
